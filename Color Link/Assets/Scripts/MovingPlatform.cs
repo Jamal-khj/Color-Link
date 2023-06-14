7aae8fb3bbb4b02fb5fcdabc1e7ec213dbee9319
Jamal-khj <114579024+Jamal-khj@users.noreply.github.com>
2023-06-14 14:31:46 +0400

    Moving Platform Verticle

--- Color Link/Assets/Scripts/MovingPlatform.cs
+++ Color Link/Assets/Scripts/MovingPlatform.cs
@@ -18,7 +18,7 @@ public class MovingPlatform : MonoBehaviour
     {
         if(Vector2.Distance(transform.position, PosA.position) < 0.1f) targetPos = PosB.position;
 
-        if(Vector2.Distance(transform.position, PosB.position) < 0.1f) targetPos = PosA.position;
+        if (Vector2.Distance(transform.position, PosB.position) < 0.1f) targetPos = PosA.position;
 
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
     }
